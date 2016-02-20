8992968d062d99a402c254ac63597f3dc518cc4a
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-20 12:10:03 +0000

    Update 2016-02-20T12:09:46.342Z

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 0.1.1
-
-- Enable to require as a npm package.
-
-## 0.1.0
-
-- Initial release.
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import Textcomplete from './textcomplete';
-import Textarea from './textarea';
-
-global.Textcomplete = Textcomplete;
-global.Textcomplete.editors = {
-  Textarea: Textarea,
-};
